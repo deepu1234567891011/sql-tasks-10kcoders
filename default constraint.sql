9adb10221b388b7fe066f8cadb3eb3fed7456e03
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-13 03:02:30 +0530

    Add files via upload

---
+++ default constraint.sql
@@ -0,0 +1,5 @@
+create table studen(stu_id int,stu_name varchar(60)not null, stu_city varchar(70) DEFAULT "Guntur" );
+insert into studen values(1,"sri","Hyd");
+insert into studen values(2,"Sushu","vijaywada");
+insert into studen values(3,"swpna");
+select * from studen;
