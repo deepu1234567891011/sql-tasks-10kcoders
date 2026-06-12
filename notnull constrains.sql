24848785fa2695d3793267deeb9312f928587356
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-13 02:11:42 +0530

    Add files via upload

---
+++ notnull constrains.sql
@@ -0,0 +1,5 @@
+create table students(stu_id int,name varchar(60)not null );
+insert into students values(1,"sri");
+insert into students values(2,"Sushu");
+insert into students values(3,"swpna");
+select * from students
